06fa8b818096b01bfe3b405fa49a0091b8c9d080
git <git@github.com>
2020-11-15 17:07:06 +0000

    store collection data into firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,14 +3,6 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  // apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  // authDomain: 'crwn-db.firebaseapp.com',
-  // databaseURL: 'https://crwn-db.firebaseio.com',
-  // projectId: 'crwn-db',
-  // storageBucket: 'crwn-db.appspot.com',
-  // messagingSenderId: '850995411664',
-  // appId: '1:850995411664:web:7ddc01d597846f65'
-
   apiKey: "AIzaSyBz3jeuPY1evgmPRFOTFVWUJMEzZpEQYVA",
   authDomain: "crwn-db-84d26.firebaseapp.com",
   databaseURL: "https://crwn-db-84d26.firebaseio.com",
@@ -39,7 +31,7 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
         displayName,
         email,
         createdAt,
-        ...additionalData
+        ...additionalData,
       });
     } catch (error) {
       console.log('error creating user', error.message);
@@ -47,6 +39,39 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   }
 
   return userRef;
+};
+
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = firestore.collection(collectionKey);
+
+  const batch = firestore.batch();
+  objectsToAdd.forEach(obj => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  });
+
+  return await batch.commit();
+};
+
+export const convertCollectionsSnapshotToMap = (collections) => {
+  const transformedCollection = collections.docs.map((doc) => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items,
+    };
+  });
+
+  return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {});
 };
 
 export const auth = firebase.auth();
