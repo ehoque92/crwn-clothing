97a57be607c8314042e6db1abe0894d706885985
git <git@github.com>
2020-11-23 04:30:55 +0000

    created cart sagasfor sign in, sign up and sign with google

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -3,7 +3,7 @@ import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 
-import { fetchCollectionsStartAsync } from '../../redux/shop/shop.actions';
+import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
 
 import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container';
 import CollectionPageContainer from '../collection/collection.container';
@@ -11,8 +11,8 @@ import CollectionPageContainer from '../collection/collection.container';
 
 class ShopPage extends React.Component{
   componentDidMount(){
-    const {fetchCollectionsStartAsync} = this.props;
-    fetchCollectionsStartAsync();
+    const {fetchCollectionsStart} = this.props;
+    fetchCollectionsStart();
 
   };
     
@@ -36,7 +36,7 @@ class ShopPage extends React.Component{
 
 
 const mapToDispatchToProps = dispatch => ({
-  fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync())
+  fetchCollectionsStart: () => dispatch(fetchCollectionsStart())
 });
 
 export default connect(null, mapToDispatchToProps)(ShopPage);
